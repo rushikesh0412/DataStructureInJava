ca642407353f97d2d62fc16f550889bd3ab7982b
Rushi <rushikeshshinde9208@gmail.com>
2026-08-14 21:41:26 +0530

    Add ArrayReverse example.

---
+++ 01_Arrays/src/com/rushi/arrays/ArrayReverse.java
@@ -0,0 +1,15 @@
+package com.rushi.arrays;
+
+public class ArrayReverse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+		
+		for(int i = numbers.length - 1; i >= 0; i--) {
+			System.out.println(numbers[i]);
+		}
+
+	}
+
+}
