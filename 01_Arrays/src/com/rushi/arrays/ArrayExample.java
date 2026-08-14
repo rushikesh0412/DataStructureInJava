f9ef2f9b013972cef6fde9b131565b5791d15712
Rushi <rushikeshshinde9208@gmail.com>
2026-08-14 21:20:03 +0530

    Add array example

---
+++ 01_Arrays/src/com/rushi/arrays/ArrayExample.java
@@ -0,0 +1,17 @@
+package com.rushi.arrays;
+
+public class ArrayExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers = {1,2,3,4,5,6,7};
+		
+		System.out.println("Array Elements: ");
+		
+		for(int i=0; i<numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+
+	}
+
+}
