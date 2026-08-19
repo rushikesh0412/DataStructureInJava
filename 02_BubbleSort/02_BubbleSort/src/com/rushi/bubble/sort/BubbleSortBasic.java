3004712a1edd46bf155cd644a8ae913581534a11
Rushi <rushikeshshinde9208@gmail.com>
2026-08-20 00:07:18 +0530

    Basic Bubble Sort — Ascending order

---
+++ 02_BubbleSort/02_BubbleSort/src/com/rushi/bubble/sort/BubbleSortBasic.java
@@ -0,0 +1,32 @@
+package com.rushi.bubble.sort;
+
+public class BubbleSortBasic {
+	static void printArray(int[] numbers) {
+		for(int num : numbers) {
+			System.out.print(num + " ");
+		}
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] numbers = {64, 34, 25, 12, 22, 11, 90};
+		
+		System.out.println("Before Srting: ");
+		printArray(numbers);
+		
+		for(int i=0; i<numbers.length - 1; i++) {
+			for(int j = 0; j<numbers.length - 1; j++) {
+				if(numbers[j] > numbers[j+1]) {
+					int temp = numbers[j];
+					numbers[j] = numbers[j+1];
+					numbers[j+1] = temp;
+				}
+			}
+		}
+		
+		System.out.println("After Srting: ");
+		printArray(numbers);
+
+	}
+
+}
